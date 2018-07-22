7ae44459bdde83ce1da8ff2549e41f69f89695bb
Oracking <Elarrick1@gmail.com>
2018-07-22 00:23:41 +0000

    Added bytes to string conversion of pipe output and an extra condition to check if controller has been detected to make code compatible with python3

--- xbox.py
+++ xbox.py
@@ -12,13 +12,13 @@ Example usage:
 
     import xbox
     joy = xbox.Joystick()         #Initialize joystick
-    
+
     if joy.A():                   #Test state of the A button (1=pressed, 0=not pressed)
         print 'A button pressed'
     x_axis   = joy.leftX()        #X-axis of the left stick (values -1.0 to 1.0)
     (x,y)    = joy.leftStick()    #Returns tuple containing left X and Y axes (values -1.0 to 1.0)
     trigger  = joy.rightTrigger() #Right trigger position (values 0 to 1.0)
-    
+
     joy.close()                   #Cleanup before exit
 """
 
@@ -34,7 +34,7 @@ class Joystick:
     The refreshRate determines the maximnum rate at which events are polled from xboxdrv.
     Calling any of the Joystick methods will cause a refresh to occur, if refreshTime has elapsed.
     Routinely call a Joystick method, at least once per second, to avoid overfilling the event buffer.
- 
+
     Usage:
         joy = xbox.Joystick()
     """
@@ -55,11 +55,17 @@ class Joystick:
             readable, writeable, exception = select.select([self.pipe],[],[],0)
             if readable:
                 response = self.pipe.readline()
+                # Python 3 returns response in bytes. We need to convert to str
+                if isinstance(response, bytes):
+                    response = response.decode('utf-8')
                 # Hard fail if we see this, so force an error
                 if response[0:7] == 'No Xbox':
                     raise IOError('No Xbox controller/receiver found')
                 # Success if we see the following
                 if response[0:12].lower() == 'press ctrl-c':
+                    found = True
+                # Success if we see the following for python3
+                if response[0:8].lower() == 'usb path':
                     found = True
                 # If we see 140 char line, we are seeing valid input
                 if len(response) == 140:
@@ -85,11 +91,15 @@ class Joystick:
                 # Read every line that is availabe.  We only need to decode the last one.
                 while readable:
                     response = self.pipe.readline()
+                    # Convert from bytes to str for python3
+                    if isinstance(response, bytes):
+                        response = response.decode('utf-8')
                     # A zero length response means controller has been unplugged.
                     if len(response) == 0:
                         raise IOError('Xbox controller disconnected from USB')
                     readable, writeable, exception = select.select([self.pipe],[],[],0)
-                # Valid controller response will be 140 chars.  
+
+                # Valid controller response will be 140 chars.
                 if len(response) == 140:
                     self.connectStatus = True
                     self.reading = response
@@ -101,7 +111,7 @@ class Joystick:
     controller inputs will stop updating, so the last readings will remain in effect.  It is
     good practice to only act upon inputs if the controller is connected.  For instance, for
     a robot, stop all motors if "not connected()".
-    
+
     An inital controller input, stick movement or button press, may be required before the connection
     status goes True.  If a connection is lost, the connection will resume automatically when the
     fault is corrected.
@@ -149,22 +159,22 @@ class Joystick:
     def dpadUp(self):
         self.refresh()
         return int(self.reading[45:46])
-        
+
     # Dpad Down status - returns 1 (pressed) or 0 (not pressed)
     def dpadDown(self):
         self.refresh()
         return int(self.reading[50:51])
-        
+
     # Dpad Left status - returns 1 (pressed) or 0 (not pressed)
     def dpadLeft(self):
         self.refresh()
         return int(self.reading[55:56])
-        
+
     # Dpad Right status - returns 1 (pressed) or 0 (not pressed)
     def dpadRight(self):
         self.refresh()
         return int(self.reading[60:61])
-        
+
     # Back button status - returns 1 (pressed) or 0 (not pressed)
     def Back(self):
         self.refresh()
@@ -194,7 +204,7 @@ class Joystick:
     def A(self):
         self.refresh()
         return int(self.reading[100:101])
-        
+
     # B button status - returns 1 (pressed) or 0 (not pressed)
     def B(self):
         self.refresh()
@@ -224,7 +234,7 @@ class Joystick:
     def leftTrigger(self):
         self.refresh()
         return int(self.reading[129:132]) / 255.0
-        
+
     # Right trigger value scaled between 0.0 to 1.0
     def rightTrigger(self):
         self.refresh()
@@ -239,7 +249,7 @@ class Joystick:
 
     # Returns tuple containing X and Y axis values for Right stick scaled between -1.0 to 1.0
     # Usage:
-    #     x,y = joy.rightStick() 
+    #     x,y = joy.rightStick()
     def rightStick(self,deadzone=4000):
         self.refresh()
         return (self.rightX(deadzone),self.rightY(deadzone))
